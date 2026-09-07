8734028343627c30bc20bf1ec8dd602691cdadfc
mikki <mi_hau94@hotmail.com>
2026-09-07 18:49:51 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import './App.css'
+import {Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import {fetchNflTeamData} from "./fetch.js";
+import {ppsCalculate, type Team} from "./utils.ts";
+
+type IState = {
+  teams: Team[],
+};
+
+function App() {
+  const [state, setState] = useState<IState>({
+    teams: [],
+  });
+
+  async function onClickUpdate() {
+    const teams: Team[] = await fetchNflTeamData();
+    teams.forEach(team => {
+      setState(prevState => ({
+        teams: [...prevState.teams, {...team, pps: ppsCalculate(team.wins, team.losses, team.pd, team.pf, team.pa)}],
+      }))
+    });
+  }
+
+  return (
+    <>
+      <Button onClick={() => onClickUpdate()}>Update</Button>
+      <section id="center">
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }}>
+            <TableHead>
+              <TableRow sx={{ bgcolor: '#9ca3af' }}>
+                <TableCell>Ranking</TableCell>
+                <TableCell>Team</TableCell>
+                <TableCell>Power ranking score</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {state.teams.map((team, index) => (
+                  <TableRow sx={{ bgcolor: '#9ca3af' }}>
+                    <TableCell>{index + 1}</TableCell>
+                    <TableCell>{team.name}</TableCell>
+                    <TableCell>{team.pps}</TableCell>
+                  </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/fetch.js
@@ -0,0 +1,23 @@
+export async function fetchNflTeamData() {
+    const response = await fetch('https://site.api.espn.com/apis/v2/sports/football/nfl/standings');
+    const conferences = await response.json().then(data => data?.children);
+    const teams= [];
+
+    if (conferences !== null && conferences !== undefined) {
+        conferences.map(conference => {
+            conference?.standings?.entries.forEach((entry) => {
+                teams.push({
+                    name: entry.team.displayName || entry.team.name,
+                    wins: entry.stats.find(stat => stat.name === "wins").value,
+                    losses: entry.stats.find(stat => stat.name === "losses").value,
+                    pa: entry.stats.find(stat => stat.name === "pointsAgainst").value,
+                    pf: entry.stats.find(stat => stat.name === "pointsFor").value,
+                    pd: entry.stats.find(stat => stat.name === "pointDifferential").value,
+                    pps: 0,
+                })
+            })
+        });
+    }
+
+    return teams;
+}
